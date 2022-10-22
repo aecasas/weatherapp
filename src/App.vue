d98dbcf3861d53314eaf243f4fcca752b12d6014
aecasas <angelcasas94@gmail.com>
2022-10-21 22:04:56 -0400

    Added Geolocation

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,22 @@
       class="search-bar" 
       placeholder="Search..."
       v-model="query"
-      @keypress="fetchWeather"
+      @keypress="getWeather"
       />
-      
-    
-
     </div>
 
+        <div class="location-box">
+          <div class="location">{{ weather.name }}, {{  }}</div>
+        <div class="date">Today</div>
+      </div>
+    <div class="weather-box">
+      <div class="temp">weather.</div>
+        <div class="weather">{{weather.name}}</div>
+        <div class="coordinates">{{coordinates.lat}} Latitude, {{coordinates.lng}} Longitude</div></div>
+
+<p>please allow location services</p>
     <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+    <p>Please allow location services</p>
         <div class="location-box">
           <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
         <div class="date">{{dateBuilder()}}</div>
@@ -21,36 +29,95 @@
     <div class="weather-box">
       <div class="temp">{{ Math.round(weather.main.temp) }}°c </div>
         <div class="weather">{{weather.weather[0].main}}</div>
-      
-      
+        <div class="coordinates">{{coordinates.lat}} Latitude, {{coordinates.lng}} Longitude</div>
+      <!-- Rounded switch -->
+
+      <div class="slider-wrap">
+<label class="switch">
+  <input type="checkbox">
+  <span class="slider round"></span>
+</label>
+</div>
+
+
     </div>
     </div>
 
     </main>
+    <myLocation></myLocation>
   </div>
 </template>
 
 <script>
+
+import myLocation from './components/myLocation'
+
+
+
 export default {
   name: 'App',
+  components: {
+    myLocation
+  },
+  //weather api 
   data (){
     return {
       api_key: '8690c3d68f5af087d8daad88c04c4fcd',
       url_base: 'https://api.openweathermap.org/data/2.5/',
       query: '',
-      weather:{}
+      weather:{},
+      coordinates:{
+        lat: 0,
+        long: 0
+      }
     }
   },
+  //fetch geolocation
+  created(){
+
+    this.$getLocation({})
+    .then(coordinates => {
+      this.coordinates = coordinates
+    })
+    .catch(error => alert(error))
+  },
+  //testing pressing enter to fetch data
   methods: {
-    fetchWeather(e){
+
+    getWeather(){
+
+
+    this.$getLocation({})
+    .then(coordinates => {
+      this.coordinates = coordinates
+    })
+
+    
+    fetch(`${this.url_base}weather?lat=${this.coordinates.lat}&lon=${this.coordinates.lon}&units=metric&APPID=${this.api_key}`)
+    
+        .then(res => {
+          return res.json();
+        }).then(this.setResults);
+
+
+
+    },
+
+
+
+
+/*     fetchWeather(e){
       if (e.key == "Enter") {
-        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+        //I need to be able to fetch the api using this: 
+        
+        //fetch(`${this.url_base}weather?lat=${this.coordinates.lat}&lon=${this.coordinates.lon}&units=metric&APPID=${this.api_key}`)
+        fetch(`${this.url_base}weather?q=${this.query}}&units=metric&APPID=${this.api_key}`)
         .then(res => {
           return res.json();
         }).then(this.setResults);
       }
     },
-
+ */
     setResults (results){
       this.weather = results;
     },
@@ -169,6 +236,68 @@ main {
   font-weight: 700;
   font-style: italic;
   text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
 }
 
 </style>
---
+++ src/components/myLocation.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <div>{{coordinates.lat}} Latitude, {{coordinates.lng}} Longitude</div>
+      
+    </div>
+</template>
+
+<script>
+
+</script>
